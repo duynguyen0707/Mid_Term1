83815e3bfafcdddc8e7e9369fccedc8c8bd6b9b8
Duy <kieuduy13@gmail.com>
2021-09-06 23:09:24 +0700

    Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,18 @@
 public class App {
-    public static void main (String [] args){
+    public static void main(String[] args) {
 //        Câu 1
 //        Cho chuỗi sau: You Only Live Once. But if You do it right. once is Enough
-////        Đếm số từ viết in hoa trong chuỗi (VD: chuỗi trên có 7 ký tự in hoa)
+//       Đếm số từ viết in hoa trong chuỗi (VD: chuỗi trên có 7 ký tự in hoa)
 //        Viết in hoa toàn bộ chữ đầu của các từ trong chuỗi đó (VD: You Only Live Once. But If You Do It Right. Once Is Enough)
 //        Chuyển ký tự đầu tiên của chuỗi hoặc ký tự đầu tiên của từ sau dấu chấm là viết in hoa, các chữ con lại viết thường (VD: You only live once. But if you do it right. Once is enough)
         String s = "You Only Live Once. But if You do it right. once is Enough";
 
         int n = EntryPoint.countUpperCaseInString(s);
-        System.out.println("Total nummber of uppcase in strings is: " +n);
+        System.out.println("Total nummber of uppcase in strings is: " + n);
+
         String str = EntryPoint.upperCaseAllFirstLetter(s);
         System.out.println(str);
-        String str1 = EntryPoint.upperCaseFirstLetterAndAfterMaker(s);
+        String str1 = EntryPoint.upperCaseFirsletterAfterPeriod(s);
         System.out.println(str1);
     }
 }
--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,42 +1,84 @@
 import java.util.Locale;
 
 public class EntryPoint {
+
     public static int countUpperCaseInString(String str) {
+
         int n = 0;
 
         for (int i = 0; i < str.length(); i++) {
+
             if (Character.isUpperCase(str.charAt(i))) {
-                n++;
+
+                n += 1;
             }
         }
+
         return n;
     }
 
+    // Viết in hoa toàn bộ chữ đầu của các từ trong chuỗi đó (VD: You Only Live Once. But If You Do It Right. Once Is Enough)
+
     public static String upperCaseAllFirstLetter(String str) {
-        char[] array = str.toCharArray();
-        array[0] = Character.toUpperCase(array[0]);
 
-        for (int i = 1; i < array.length; i++) {
-            if (Character.isWhitespace(array[i - 1])) {
-                array[i] = Character.toUpperCase(array[i]);
+
+        char[] arr = str.toCharArray();
+
+        char fr = arr[0];
+
+        char frUpper = Character.toUpperCase(fr);
+
+        arr[0] = frUpper;
+
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (Character.isWhitespace(arr[i])) {
+
+                char c = arr[i + 1];
+
+                char inHoa = Character.toUpperCase(c);
+
+                arr[i + 1] = inHoa;
             }
+
         }
 
-        return new String(array);
+        String result = String.valueOf(arr);
+
+        return result;
     }
 
-    public static String upperCaseFirstLetterAndAfterMaker(String str) {
-        String s = str.toLowerCase();
-        char[] array = s.toCharArray();
 
-        for (int i = 1; i < array.length; i++) {
-            array[0] = Character.toUpperCase(array[0]);
+    public static String upperCaseFirsletterAfterPeriod(String str) {
 
-            if (Character.valueOf(array[i]) == '.') {
-                array[i + 2] = Character.toUpperCase(array[i + 2]);
+        String str1 = str.toLowerCase();
+
+        char[] arr = str1.toCharArray();
+
+        char c = arr[0];
+
+        char cUpperCase = Character.toUpperCase(c);
+
+        arr[0] = cUpperCase;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (Character.valueOf(arr[i]).equals('.')) {
+
+                char c1 = arr[i + 2];
+
+                char c1Upper = Character.toUpperCase(c1);
+
+                arr[i + 2] = c1Upper;
             }
+
         }
 
-        return new String(array);
+        String result = String.valueOf(arr);
+
+        return result;
     }
+
+
 }
